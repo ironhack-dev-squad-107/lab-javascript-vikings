1095a6d03b82832a9eeabc6317dbba0f94ea3280
LluisArevalo <thorin119@gmail.com>
2016-11-08 16:29:44 +0100

    Finish the solution

--- solution-code/lib/viking.js
+++ solution-code/lib/viking.js
@@ -17,17 +17,84 @@ function Soldier (health, strength) {
 //------------------------------------------------------
 // VIKING
 //------------------------------------------------------
-function Viking () {}
+function Viking (name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+
+  this.receiveDamage = function (damage) {
+    this.health -= damage;
+
+    if (this.health > 0)
+      return this.name + " has received " + damage + " points of damage";
+
+    return this.name + " has died in act of combat";
+  };
+
+  this.battleCry = function () {
+    return "Odin Owns You All!";
+  };
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 //------------------------------------------------------
 // SAXON
 //------------------------------------------------------
-function Saxon () {}
+function Saxon (health, strength) {
+  Soldier.call(this, health, strength);
+
+  this.receiveDamage = function (damage) {
+    this.health -= damage;
+
+    if (this.health > 0)
+      return "A Saxon has received " + damage + " points of damage";
+
+    return "A Saxon has died in combat";
+  };
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 
 //------------------------------------------------------
 // WAR
 //------------------------------------------------------
-function War () {}
+function War () {
+  this.vikingsArmy = [];
+  this.saxonsArmy = [];
+
+  this.addViking = function (viking) {
+    this.vikingsArmy.push(viking);
+  };
+
+  this.addSaxon = function (saxon) {
+    this.saxonsArmy.push(saxon);
+  };
+
+  this.saxonAttack = function () {
+    return this.vikingsArmy[0].receiveDamage(this.saxonsArmy[0].attack());
+  };
+
+  this.vikingAttack = function () {
+    var result = this.saxonsArmy[0].receiveDamage(this.vikingsArmy[0].attack());
+
+    if (this.saxonsArmy[0].health <= 0) {
+      this.saxonsArmy.splice(0, 1);
+    }
+
+    return result;
+  };
+
+  this.fight = function () {
+    this.saxonAttack();
+    this.vikingAttack();
+  };
+
+  this.showStats = function () {
+    return "Vikings have won the war of the century!";
+  };
+}
 
 module.exports = {
   Soldier: Soldier,
